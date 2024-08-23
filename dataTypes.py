af6339065f7771631078b2af0645807fff079464
Njeri Wekesa <wekesacharity@gmail.com>
2024-08-23 12:11:35 +0300

    intorduction to variables and data types in python

---
+++ dataTypes.py
@@ -0,0 +1,17 @@
+# numbers - integers, floats, complex numbers
+
+num1 = 5
+print(num1, 'is of type', type(num1))
+
+num2 = 12.4
+print (num2, 'is of type', type(num2))
+
+num3 = 1+3j
+print(num3, 'is of type', type(num3))
+
+'''
+output: 
+5 is of type <class 'int'>
+12.4 is of type <class 'float'>
+(1+3j) is of type <class 'complex'>
+'''
---
+++ variables.py
@@ -0,0 +1,16 @@
+# numbers = 10
+
+# print(numbers)
+
+# site_name = "power learn project"
+# print(site_name)
+
+# site_name = "apple.com"
+# print(site_name)
+
+# a, b, c = 1, 5, 'njeri'
+# print(a, b, c)
+# print (a)
+# print (b)
+# print (c)
+
